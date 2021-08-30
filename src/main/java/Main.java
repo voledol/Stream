058c79c59d8780710cd4510256529f7338235390
voledoler <voledoler@yandex.ru>
2021-08-31 00:06:23 +0300

    Выполнено

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Please use tests to run the program");
+    }
+   // public static List<Integer> numbers = new ArrayList<Integer>();
+
+}
---
+++ src/main/java/tasks/Task1_Basic.java
@@ -0,0 +1,76 @@
+package tasks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class Task1_Basic {
+
+    /**
+     * Функция должна вернуть количество четных чисел в списке.
+     *
+     * Пример:
+     *  0,2 -> 2
+     *  1,2,3,5 -> 1
+     *
+     * Тут подойдут эти методы:
+     *    - Collection::stream()
+     *    - Stream::filter()
+     *    - Stream::count()
+     */
+    static long countEven(Collection<Integer> numbers) {
+        int count = (int) numbers.stream().filter(integer -> integer % 2 == 0).count();
+        return count;
+    }
+
+    /**
+     * Функция должна вернуть число строчных символов в строке.
+     *
+     * Пример:
+     *  "abcDE" -> 3
+     *  "ABC" -> 0
+     *
+     * Тут подойдут эти методы:
+     *    - String::chars()
+     *    - Character::isLowerCase()
+     *    - Stream::count()
+     */
+    static long countLowercaseLetters(String str) {
+        long count = str.chars().filter(Character::isLowerCase).count();
+
+        return count;
+
+
+    }
+
+
+
+
+    /**
+     * Функция должна заменить каждое слово в строке его длинной.
+     *
+     * Слова разделяются одним или более пробелами.
+     *
+     * Пример:
+     *   "a b cd" -> "1 1 2"
+     *   "one two   three" -> "3 3 5"
+     *
+     * Тут подойдут эти методы:
+     *    - String::split()
+     *    - Stream::map()
+     *    - Stream::collect()
+     *    - Collectors.joining()
+     */
+    static String replaceWordsOnLength(String str) {
+        String[] array = str.split("\s++");
+        return Arrays.stream(array).map(s -> {
+            int lenght = s.length();
+            s.replaceAll("^\\s", "");
+            s = ""+lenght;
+            return s;
+        }).collect(Collectors.joining(" "));
+    }
+}
